6f44042bad41930a24756609595e3e5d32ba5441
cannedcorgies <artistafdo@gmail.com>
2025-05-30 08:18:20 -0700

    starting over...

--- src/App.js
+++ src/App.js
@@ -1,12 +1,9 @@
 import React from "react";
-import Slideshow from "./components/Slideshow";
-import GameGrid from "./components/GameGrid";
 
 function App() {
   return (
     <main className="bg-white text-gray-900">
-      <Slideshow />
-      <GameGrid />
+
     </main>
   );
 }
